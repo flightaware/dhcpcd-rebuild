407d45f1399dfedf29f197979b3e620eae7603dc
Oliver Jowett <oliver.jowett@flightaware.com>
2020-03-12 19:16:47 +0800

    Add build automation

---
+++ prepare-build.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+export DEBFULLNAME=${DEBFULLNAME:-FlightAware build automation}
+export DEBEMAIL=${DEBEMAIL:-adsb-devs@flightaware.com}
+
+TOP=$(dirname $0)
+DIST=$1
+OUT=$2
+
+if [ -n "$OUT" ]
+then
+    OUT=$(realpath $OUT)
+else
+    OUT=$(realpath package-$DIST)
+fi
+
+cp -a $TOP/dhcpcd5-8.1.2 $OUT
+cd $OUT
+
+case "$DIST" in
+    buster)
+        ;;
+    *)
+        echo "Don't know how to build tcltls for a distribution named $DIST" >&2
+        exit 1
+        ;;
+esac
